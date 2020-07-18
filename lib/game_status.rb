f5cc4d2e461226f9ce6f32aa54283e72d64e06bf
Joan' Araujo <wisdombthegame@gmail.com>
2020-07-18 23:23:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,59 +1,48 @@
+
 WIN_COMBINATIONS = [
-  [0,1,2],
+  [0,1,2], 
   [3,4,5],
   [6,7,8],
   [0,3,6],
   [1,4,7],
   [2,5,8],
-  [0,4,8],
-  [6,4,2]
+  [2,4,6],
+  [0,4,8]
+  
 ]
 
-
-
-
-  def position_taken?(board, index)
+ def position_taken?(board, index)
     !(board[index].nil? || board[index] == " ")
   end
-
-def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
-
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+  
+  #WON?: CHECKS A BOARD AND RETURNS TRUE IF THERE IS A WIN 
+  #AND FALSE IF NOT.
+  #RETURN WINNING_COMBINATION AS AN ARRAY IF THERE IS A WINNING_COMBINATION.
+  #RETURNS HOW THEY WON BY MEANS OF WINNING WINNING_COMBINATION
+  #ITERATE OVER THE WIN_COMBINATIONS AND CHECK IF BOARD HAS 
+  #THE SAME PLAYER TOKE IN EACH INDEX OF A WINNNING COMBINATIOn
+  #expected a collection that can be converted to an array with `#to_ary` or `#to_a`, but got nil
+  
+    def won?(board)
+      WIN_COMBINATIONS.each do |win_combination| 
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+     
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      
+        
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return win_combination
     end
-  }
+  end
   return false
 end
 
-def full?(board)
-  board.all? {|index| index == "X" || index == "O"}
-  end
-  
-  
-def draw?(board)
- !won?(board)  && full?(board)
-  end
 
-
-def over?(board)
-  won?(board) && draw?(board) || full?(board)
-end
- 
-def winner(board)
-  win_combo = won?(board)
-  if won 
-    return board[won[0]]
-  end
-end
- 
+      
+      
+      
  
